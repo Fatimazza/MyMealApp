4fc439b9141a30bf0d44c8a2d01884238e2a82a5
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 01:15:30 +0700

    Meal Home Screen - Add Photo Card containing Async Image to display a single photo

--- app/src/main/java/id/fatimazza/mymealapp/ui/HomeScreen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/HomeScreen.kt
@@ -5,17 +5,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import id.fatimazza.mymealapp.R
+import id.fatimazza.mymealapp.model.MealsItem
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
 
 @Composable
@@ -43,6 +49,21 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
         modifier = modifier.size(200.dp),
         painter = painterResource(R.drawable.loading_image),
         contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun MealPhotoCard(meals: MealsItem, modifier: Modifier = Modifier) {
+    AsyncImage(
+        model = ImageRequest.Builder(context = LocalContext.current)
+            .data(meals.strMealThumb)
+            .crossfade(true)
+            .build(),
+        contentDescription = stringResource(R.string.meals_photo),
+        contentScale = ContentScale.Crop,
+        error = painterResource(R.drawable.ic_broken_image),
+        placeholder = painterResource(R.drawable.loading_image),
+        modifier = Modifier.fillMaxWidth()
     )
 }
 

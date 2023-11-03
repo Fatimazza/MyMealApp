4bcae101397e06578ff209651d2d6ff64bfe8151
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 16:03:00 +0700

    Meal View Model - Call Get Meals function on the background

--- app/src/main/java/id/fatimazza/mymealapp/ui/MealsViewModel.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/MealsViewModel.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.fatimazza.mymealapp.network.MealsApi
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class MealsViewModel() : ViewModel() {
 
@@ -18,8 +22,14 @@ class MealsViewModel() : ViewModel() {
     /**
      * Gets Meals information from the Meals API Retrofit service
      */
-    fun getMealsData() {
-        mealsUiState = "Set the Mars API status response here!"
-    }
+    private fun getMealsData() {
+        viewModelScope.launch {
+            try {
+                val listResult = MealsApi.retrofitService.getMeals()
+                mealsUiState = listResult
+            } catch (e: IOException) {
 
+            }
+        }
+    }
 }

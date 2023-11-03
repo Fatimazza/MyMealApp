54ea37a0742298e41d0f067d656a24ee883d3dd6
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 11:42:00 +0700

    My Meal App - Move Scaffold and Bottom Navigation Bar from Home to Meal App

--- app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
@@ -1,14 +1,84 @@
 package id.fatimazza.mymealapp.ui
 
 import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import id.fatimazza.mymealapp.R
+import id.fatimazza.mymealapp.ui.navigation.NavigationItem
+import id.fatimazza.mymealapp.ui.navigation.Screen
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MyMealApp() {
-    HomeScreen()
+fun MyMealApp(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_title)) },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        },
+        bottomBar = {
+            BottomBar()
+        },
+        modifier = modifier
+    ) {
+    }
+}
+
+@Composable
+private fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    val navigationItems = listOf(
+        NavigationItem(
+            title = stringResource(R.string.menu_home),
+            icon = Icons.Default.Home,
+            screen = Screen.Home
+        ),
+        NavigationItem(
+            title = stringResource(R.string.menu_favorite),
+            icon = Icons.Default.Favorite,
+            screen = Screen.Favorite
+        )
+    )
+    NavigationBar(
+        modifier = modifier,
+    ) {
+        for (navItem in navigationItems) {
+            NavigationBarItem(
+                selected = false,
+                onClick = { },
+                icon = {
+                    Icon(
+                        imageVector = navItem.icon,
+                        contentDescription = navItem.title
+                    )
+                },
+                label = { Text(navItem.title) },
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)

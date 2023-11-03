c50e3d243561f0d7a400dde2d6c7bf6851dfae79
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 11:56:54 +0700

    My Meal App - Change selected Menu on Bottom Navigation

--- app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
@@ -14,11 +14,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import id.fatimazza.mymealapp.R
 import id.fatimazza.mymealapp.ui.navigation.MealNavHost
@@ -57,6 +59,9 @@ private fun BottomBar(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     val navigationItems = listOf(
         NavigationItem(
             title = stringResource(R.string.menu_home),
@@ -74,7 +79,7 @@ private fun BottomBar(
     ) {
         for (navItem in navigationItems) {
             NavigationBarItem(
-                selected = false,
+                selected = currentRoute == navItem.screen.route,
                 onClick = {
                     navController.navigate(navItem.screen.route) {
                         popUpTo(navController.graph.findStartDestination().id) {

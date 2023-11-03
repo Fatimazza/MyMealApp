c6b092f64738ce619abb3cc633f0620dd766cae0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 23:04:08 +0700

    Network - Add initial Meals Repository

---
+++ app/src/main/java/id/fatimazza/mymealapp/data/MealsRepository.kt
@@ -0,0 +1,8 @@
+package id.fatimazza.mymealapp.data
+
+/**
+ * Repository that fetch meals list from Meals Api.
+ */
+interface MealsRepository {
+    suspend fun getMarsPhotos(): List<MealsRepository>
+}

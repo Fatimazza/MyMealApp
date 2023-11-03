522344d1553852371cab49adbba944d9ef03a8b0
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 23:20:17 +0700

    Network - Add Network Implementation in Meals Repository

--- app/src/main/java/id/fatimazza/mymealapp/data/MealsRepository.kt
+++ app/src/main/java/id/fatimazza/mymealapp/data/MealsRepository.kt
@@ -1,8 +1,21 @@
 package id.fatimazza.mymealapp.data
 
+import id.fatimazza.mymealapp.model.MealsItem
+import id.fatimazza.mymealapp.network.MealsApi
+import id.fatimazza.mymealapp.network.MealsApiService
+
 /**
  * Repository that fetch meals list from Meals Api.
  */
 interface MealsRepository {
-    suspend fun getMarsPhotos(): List<MealsRepository>
+    suspend fun getMealsData(): List<MealsItem>
+}
+
+/**
+ * Network Implementation of Repository that fetch Meals list from Meals Api.
+ */
+class NetworkMealsRepository() : MealsRepository {
+    override suspend fun getMealsData(): List<MealsItem> {
+        return MealsApi.retrofitService.getMeals().meals
+    }
 }

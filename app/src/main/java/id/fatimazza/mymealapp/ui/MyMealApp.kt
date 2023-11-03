26da9a87614ac80aba305e2ca4faaf13b7db8377
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 10:01:13 +0700

    My Meal App - Add new blank Screen and remove unused Composables

---
+++ app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
@@ -0,0 +1,18 @@
+package id.fatimazza.mymealapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
+
+@Composable
+fun MyMealApp() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyMealAppPreview() {
+    MyMealAppTheme {
+        MyMealApp()
+    }
+}
+

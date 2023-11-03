7f5c18608e2b58dccb5d4af9083bfe4f18b2f37b
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 00:09:45 +0700

    Meals Application - Attach Meals App Container to the Application

---
+++ app/src/main/java/id/fatimazza/mymealapp/MyMealsApplication.kt
@@ -0,0 +1,15 @@
+package id.fatimazza.mymealapp
+
+import android.app.Application
+import id.fatimazza.mymealapp.data.DefaultAppContainer
+import id.fatimazza.mymealapp.data.MealsAppContainer
+
+class MyMealsApplication : Application() {
+    /** AppContainer instance used by the rest of classes to obtain dependencies */
+    lateinit var container: MealsAppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
+

736fcb4160f27c81d5ecd79f342dd4a59d699b51
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 00:29:13 +0700

    Network - Remove moved codes in Meals API Service

--- app/src/main/java/id/fatimazza/mymealapp/network/MealsAPIService.kt
+++ app/src/main/java/id/fatimazza/mymealapp/network/MealsAPIService.kt
@@ -1,22 +1,7 @@
 package id.fatimazza.mymealapp.network
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import id.fatimazza.mymealapp.model.MealsResponse
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import retrofit2.http.GET
-
-private const val BASE_URL =
-    "https://www.themealdb.com/api/json/"
-
-/**
- * Use the Retrofit builder to build a retrofit object
- */
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-    .baseUrl(BASE_URL)
-    .build()
 
 /**
  * Retrofit service object for creating api calls
@@ -24,14 +9,5 @@ private val retrofit = Retrofit.Builder()
 interface MealsApiService {
     @GET("v1/1/filter.php?a=Japanese")
     suspend fun getMeals(): MealsResponse
-}
-
-/**
- * A public Api object that exposes the lazy-initialized Retrofit service
- */
-object MealsApi {
-    val retrofitService: MealsApiService by lazy {
-        retrofit.create(MealsApiService::class.java)
-    }
 }
 

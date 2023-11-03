7895b931ba0005b6b20409cf2f8aa798fab8c845
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 23:40:03 +0700

    Network - Move Retrofit object and its initialization in App Container class

--- app/src/main/java/id/fatimazza/mymealapp/data/MealsAppContainer.kt
+++ app/src/main/java/id/fatimazza/mymealapp/data/MealsAppContainer.kt
@@ -1,5 +1,11 @@
 package id.fatimazza.mymealapp.data
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import id.fatimazza.mymealapp.network.MealsApiService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 /**
  * Dependency Injection container at the application level.
  */
@@ -13,6 +19,25 @@ interface MealsAppContainer {
  * Variables are initialized lazily and the same instance is shared across the whole app.
  */
 class DefaultAppContainer : MealsAppContainer {
+
+    private val BASE_URL =
+        "https://www.themealdb.com/api/json/"
+
+    /**
+     * Use the Retrofit builder to build a retrofit object
+     */
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(BASE_URL)
+        .build()
+
+    /**
+     * A public Api object that exposes the lazy-initialized Retrofit service
+     */
+    private val retrofitService: MealsApiService by lazy {
+            retrofit.create(MealsApiService::class.java)
+        }
+
     override val mealsRepository: MealsRepository
         get() = TODO("Not yet implemented")
 

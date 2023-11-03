3300385181ba4ac91d2feecf2e66111e8a2db9fc
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-03 20:08:58 +0700

    Meal Home Screen - Add Loading Screen

--- app/src/main/java/id/fatimazza/mymealapp/ui/HomeScreen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/HomeScreen.kt
@@ -1,30 +1,46 @@
 package id.fatimazza.mymealapp.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.fatimazza.mymealapp.R
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
 
 @Composable
 fun HomeScreen(
-    mealsUiState: MealsUiState
+    mealsUiState: MealsUiState,
+    modifier: Modifier = Modifier
 ) {
     when (mealsUiState) {
-        is MealsUiState.Success ->
-            HomeResultScreen(
-                mealsUiState.meals
-            )
-
-        else -> {
+        is MealsUiState.Loading ->
+            LoadingScreen(modifier.fillMaxSize())
+        is MealsUiState.Success -> {
             HomeResultScreen(
                 mealsUiState.toString()
             )
         }
+        is MealsUiState.Error -> {
+            LoadingScreen(modifier.fillMaxSize())
+        }
     }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_image),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
 
 @Composable

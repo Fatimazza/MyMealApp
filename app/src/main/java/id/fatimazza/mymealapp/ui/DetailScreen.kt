6ea55e6c507bf7f0eea094e97cb60b88c1ddd1ee
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 05:44:47 +0700

    Meal Detail Screen - Add initial Screen and Preview

---
+++ app/src/main/java/id/fatimazza/mymealapp/ui/DetailScreen.kt
@@ -0,0 +1,19 @@
+package id.fatimazza.mymealapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
+
+@Composable
+fun DetailScreen(
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailScreenPreview() {
+    MyMealAppTheme {
+        DetailScreen()
+    }
+}

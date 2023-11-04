06f59fabf5b04ff963651a0a0681569a6fca3627
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 07:59:26 +0700

    Navigation - Add Detail Menu in Screen class

--- app/src/main/java/id/fatimazza/mymealapp/ui/navigation/Screen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/navigation/Screen.kt
@@ -3,4 +3,7 @@ package id.fatimazza.mymealapp.ui.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Favorite : Screen("favorite")
+    object DetailMenu : Screen("home/{menuId}") {
+        fun createRoute(menuId: String) = "home/$menuId"
+    }
 }

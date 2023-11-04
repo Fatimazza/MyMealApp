6f7b622174d0363a9a49c489f6391d40d2d7f069
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 07:31:53 +0700

    Meal Detail Screen - Add Async Image in Detail Content Card

--- app/src/main/java/id/fatimazza/mymealapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/screen/detail/DetailScreen.kt
@@ -1,6 +1,8 @@
 package id.fatimazza.mymealapp.ui.screen.detail
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,19 +10,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import id.fatimazza.mymealapp.R
 import id.fatimazza.mymealapp.ui.components.DetailTopBar
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
@@ -73,7 +83,23 @@ private fun DetailContentCard(
                 .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.detail_card_inner_padding))
         ) {
-            
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current)
+                    .data("image")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(R.string.meals_photo),
+                contentScale = ContentScale.Crop,
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_image),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .border(
+                        BorderStroke(4.dp, MaterialTheme.colorScheme.primary),
+                        RoundedCornerShape(15.dp)
+                    )
+                    .clip(RoundedCornerShape(15.dp))
+            )
             Spacer(
                 modifier = modifier.height(8.dp)
             )

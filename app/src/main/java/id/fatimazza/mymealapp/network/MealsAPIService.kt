b5b7ba378ce53839560b85e91697172a32e51d15
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 07:38:53 +0700

    Network - Add interface to make GET Meals Detail Request in API Service class

--- app/src/main/java/id/fatimazza/mymealapp/network/MealsAPIService.kt
+++ app/src/main/java/id/fatimazza/mymealapp/network/MealsAPIService.kt
@@ -1,7 +1,9 @@
 package id.fatimazza.mymealapp.network
 
+import id.fatimazza.mymealapp.model.DetailResponse
 import id.fatimazza.mymealapp.model.MealsResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Retrofit service object for creating api calls
@@ -9,5 +11,8 @@ import retrofit2.http.GET
 interface MealsApiService {
     @GET("v1/1/filter.php?c=Beef")
     suspend fun getMeals(): MealsResponse
+
+    @GET("v1/1/lookup.php")
+    suspend fun getRecipeDetails(@Query("i") mealId: Int): DetailResponse
 }
 

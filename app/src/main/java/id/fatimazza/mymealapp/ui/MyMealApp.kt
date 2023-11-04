aa993f72970b871b027a3d1ebcfd77efee370e98
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-04 23:35:54 +0700

    My Meal App - Remove Top App Bar

--- app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/MyMealApp.kt
@@ -6,15 +6,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -34,7 +30,6 @@ import id.fatimazza.mymealapp.ui.screen.home.HomeViewModel
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MyMealApp(
@@ -43,14 +38,6 @@ fun MyMealApp(
 ) {
     val mealsViewModel: HomeViewModel = viewModel(factory = ViewModelProvider.Factory)
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.app_title)) },
-                colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer
-                )
-            )
-        },
         bottomBar = {
             BottomBar(navController)
         },

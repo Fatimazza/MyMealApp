2a9bc9431b6e4b6faa0e2e8f34d81be92c505ea6
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-09 13:36:04 +0700

    Meal Favorite Screen - Add Top App Bar

--- app/src/main/java/id/fatimazza/mymealapp/ui/screen/favorite/FavoriteScreen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/screen/favorite/FavoriteScreen.kt
@@ -1,15 +1,25 @@
 package id.fatimazza.mymealapp.ui.screen.favorite
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import id.fatimazza.mymealapp.R
 import id.fatimazza.mymealapp.data.local.FavoriteMealsItem
 import id.fatimazza.mymealapp.ui.components.MenuItem
 import id.fatimazza.mymealapp.ui.theme.MyMealAppTheme
@@ -24,24 +34,38 @@ fun FavoriteScreen(
 /**
  * The favorite screen displaying photo grid.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteGridScreen(
     meals: List<FavoriteMealsItem>,
     modifier: Modifier = Modifier,
     navigateToDetail: (String) -> Unit,
 ) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
-        modifier = modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(4.dp)
-    ) {
-        items(
-            items = meals, key = { meal -> meal.idMeal }
-        ) { meal ->
-            FavoriteMealPhotoCard(
-                meal,
-                navigateToDetail = navigateToDetail,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_title)) },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
             )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(150.dp),
+                modifier = modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(4.dp)
+            ) {
+                items(
+                    items = meals, key = { meal -> meal.idMeal }
+                ) { meal ->
+                    FavoriteMealPhotoCard(
+                        meal,
+                        navigateToDetail = navigateToDetail,
+                    )
+                }
+            }
         }
     }
 }

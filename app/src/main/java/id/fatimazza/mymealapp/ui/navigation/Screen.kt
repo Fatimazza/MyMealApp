0cd2b2dbec50c81920d28bf24303087aeb386e5d
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-11-14 12:33:49 +0700

    Navigation - Remove unused function in Screen class

--- app/src/main/java/id/fatimazza/mymealapp/ui/navigation/Screen.kt
+++ app/src/main/java/id/fatimazza/mymealapp/ui/navigation/Screen.kt
@@ -4,12 +4,6 @@ sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Favorite : Screen("favorite")
     object DetailMenu : Screen("home/{menuId}") {
-
-        var menuDetailId = ""
-        fun createMenuDetailId (menuId: String) {
-            menuDetailId = menuId
-        }
-
         fun createRoute(menuId: String) = "home/$menuId"
     }
 }
